b072354bf6f23beadb2d1f2ab95542f33411d35a
gbennum <george.bennum@gmail.com>
2017-02-08 20:30:59 -0500

    Setting up initializer

--- config/initializers/bjond_api_initializer.rb
+++ config/initializers/bjond_api_initializer.rb
@@ -141,6 +141,347 @@ config.active_definition = BjondApi::BjondAppDefinition.new.tap do |app_def|
           f.event = e.id
         end
       ]
+    end,
+    BjondApi::BjondEvent.new.tap do |e|
+      e.id = '83cd1b35-7084-4f73-84d9-2991606cf3ca'
+      e.jsonKey = 'fitnessEvent'
+      e.name = 'Fitness Event'
+      e.description = 'Validic collates data that Bjond consumes'
+      e.serviceId = app_def.id
+      e.fields = [
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '19ac2f3b-1b32-4060-b27b-9beb39d3c958'
+          f.jsonKey = 'bjondPatientId'
+          f.name = 'Patient'
+          f.description = 'The patient identifier'
+          f.fieldType = 'Person'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '7db52972-0dfc-485e-97c3-d65879330539'
+          f.jsonKey = 'type'
+          f.name = 'Activity Type'
+          f.description = 'Type of activity as provided by the device'
+          f.fieldType = 'String'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'c9f3e7c7-005e-4429-9fc5-05081b8c4642'
+          f.jsonKey = 'activityCategory'
+          f.name = 'Activity Category'
+          f.description = 'Type of activity standardized by Validic'
+          f.fieldType = 'String'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '6e4393e7-00f9-49dc-9cad-b40aed1ab00b'
+          f.jsonKey = 'intensity'
+          f.name = 'Intensity'
+          f.description = 'Subjective intensity'
+          f.fieldType = 'String'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'c6b9eb8d-705c-4d2d-beb8-dd25c38493fc'
+          f.jsonKey = 'distance'
+          f.name = 'Distance'
+          f.description = 'Distance in meters'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '1cff1590-5f59-499e-9d61-4e7b83ce3027'
+          f.jsonKey = 'duration'
+          f.name = 'Duration'
+          f.description = 'Duration in seconds'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '9143c39e-c310-4947-afdf-1f4e56e5c79a'
+          f.jsonKey = 'calories'
+          f.name = 'Calories'
+          f.description = 'Calories in kilocalories'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end
+      ]
+    end,
+    BjondApi::BjondEvent.new.tap do |e|
+      e.id = 'b6d14766-94f7-4abe-b7d5-23ee720fffff'
+      e.jsonKey = 'routineEvent'
+      e.name = 'Routine Event'
+      e.description = 'Validic collates data that Bjond consumes'
+      e.serviceId = app_def.id
+      e.fields = [
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'd27c8fdf-ad6d-4eb7-b5f3-99f45cb6745e'
+          f.jsonKey = 'bjondPatientId'
+          f.name = 'Patient'
+          f.description = 'The patient identifier'
+          f.fieldType = 'Person'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'ee63a856-878e-4c72-8be1-464f41207fb7'
+          f.jsonKey = 'steps'
+          f.name = 'Steps'
+          f.description = 'Number of steps'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '32705c22-fb48-49a6-9997-f2861c6c0e18'
+          f.jsonKey = 'distance'
+          f.name = 'Distance'
+          f.description = 'Distance in meters'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '5aadd834-f88f-4442-aab2-9f9d63d7cfce'
+          f.jsonKey = 'floors'
+          f.name = 'Floors'
+          f.description = 'Number of floors'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'c82cd944-d485-4fba-a16c-7041e7622ab6'
+          f.jsonKey = 'elevation'
+          f.name = 'Elevation'
+          f.description = 'Elevation in meters'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'bf7b10e7-2346-4692-9cbf-ccd9052cee8e'
+          f.jsonKey = 'caloriesBurned'
+          f.name = 'Calories Burned'
+          f.description = 'Calories burned in kilocalories'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '59b61674-c104-4ce7-89cc-76bedacc60fd'
+          f.jsonKey = 'water'
+          f.name = 'Water Consumption'
+          f.description = 'Water consumption in ml'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end
+      ]
+    end,
+    BjondApi::BjondEvent.new.tap do |e|
+      e.id = 'aec61c4c-6100-4a61-b05b-ee1c80902c4d'
+      e.jsonKey = 'nutritionEvent'
+      e.name = 'Nutrition Event'
+      e.description = 'Validic collates data that Bjond consumes'
+      e.serviceId = app_def.id
+      e.fields = [
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '74bd4922-ed13-4359-8949-8c64e2c3cbd6'
+          f.jsonKey = 'bjondPatientId'
+          f.name = 'Patient'
+          f.description = 'The patient identifier'
+          f.fieldType = 'Person'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'e907e4f9-abbb-40c7-8c51-c99cb506fda9'
+          f.jsonKey = 'calories'
+          f.name = 'Calories'
+          f.description = 'Calories burned in kilocalories'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'df3b81cd-f666-4384-b1dd-6157d24846dd'
+          f.jsonKey = 'carbohydrates'
+          f.name = 'Carbohydrates'
+          f.description = 'Carbohydrates in g'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'fd097764-e6e0-4d48-a479-3b530ff03638'
+          f.jsonKey = 'fat'
+          f.name = 'Fat'
+          f.description = 'Fat in g'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'c98d935d-bbf3-43ae-a7e5-72207553e72d'
+          f.jsonKey = 'fiber'
+          f.name = 'Fiber'
+          f.description = 'Fiber in g'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '1ef26666-b7d6-4357-a5c5-0b35c6fb2bcc'
+          f.jsonKey = 'protein'
+          f.name = 'Protein'
+          f.description = 'Protein in g'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '7e00776b-8202-441d-82b4-b1c6e061af47'
+          f.jsonKey = 'sodium'
+          f.name = 'Sodium'
+          f.description = 'Sodium in mg'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'a5a379dc-2cfa-4fe3-a9d7-d8062c8e3014'
+          f.jsonKey = 'water'
+          f.name = 'Water'
+          f.description = 'Water consumed in ml'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '1a4e540c-f69e-4cd3-bc64-6fd573357fcb'
+          f.jsonKey = 'meal'
+          f.name = 'Meal'
+          f.description = 'The meal, for example: pizza, Coke, chicken breast, other, unspecified'
+          f.fieldType = 'String'
+          f.event = e.id
+        end
+      ]
+    end,
+    BjondApi::BjondEvent.new.tap do |e|
+      e.id = '942a3ee7-fcf2-4b3c-b591-85de175d7383'
+      e.jsonKey = 'sleepEvent'
+      e.name = 'Sleep Event'
+      e.description = 'Validic collates data that Bjond consumes'
+      e.serviceId = app_def.id
+      e.fields = [
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'fa452551-1126-43b4-8861-0af997883234'
+          f.jsonKey = 'bjondPatientId'
+          f.name = 'Patient'
+          f.description = 'The patient identifier'
+          f.fieldType = 'Person'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'c0372a2f-1d82-4e25-9a59-8a027eac3711'
+          f.jsonKey = 'awake'
+          f.name = 'Awake'
+          f.description = 'Time awake in seconds'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'ed5901af-d890-432b-9649-f451b7a9e779'
+          f.jsonKey = 'deep'
+          f.name = 'Deep Sleep'
+          f.description = 'Time in deep sleep in seconds'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '3d53d112-cd7a-4428-9b11-abce7a941cfe'
+          f.jsonKey = 'light'
+          f.name = 'Light Sleep'
+          f.description = 'Time in light sleep in seconds'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '0098064b-7f69-4414-9de3-a2d5691e18d0'
+          f.jsonKey = 'rem'
+          f.name = 'REM Sleep'
+          f.description = 'Time in REM sleep in seconds'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'ff3d3031-87e2-42bc-a86c-79132e3409d3'
+          f.jsonKey = 'timesWoken'
+          f.name = 'Times Woken'
+          f.description = 'Number of times woken'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '863929c0-3c42-4f81-8162-609241faa352'
+          f.jsonKey = 'totalSleep'
+          f.name = 'Total Sleep'
+          f.description = 'Total sleep time in seconds'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end
+      ]
+    end,
+    BjondApi::BjondEvent.new.tap do |e|
+      e.id = '32a2434a-26ff-4c70-9079-f94189decfab'
+      e.jsonKey = 'sleepEvent'
+      e.name = 'Sleep Event'
+      e.description = 'Validic collates data that Bjond consumes'
+      e.serviceId = app_def.id
+      e.fields = [
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'cb5806e0-4204-46b7-ab91-b4b5d152461f'
+          f.jsonKey = 'bjondPatientId'
+          f.name = 'Patient'
+          f.description = 'The patient identifier'
+          f.fieldType = 'Person'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'fb0a1a30-ff42-42ec-b3c4-23e8b919bebb'
+          f.jsonKey = 'weight'
+          f.name = 'Weight'
+          f.description = 'Weight in kg'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'bb857d95-28b1-434d-adf3-91b591b19df2'
+          f.jsonKey = 'height'
+          f.name = 'Height'
+          f.description = 'Height in cm'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'fbf81924-aa16-4291-bb7b-9db2bf619c8f'
+          f.jsonKey = 'freeMass'
+          f.name = 'Free Mass'
+          f.description = 'Free mass in kg'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'f46b97bd-554f-41be-b913-99b9e893545c'
+          f.jsonKey = 'fatPercent'
+          f.name = 'Fat Percent'
+          f.description = 'Fat Percent (0-100)'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = '5ddc0242-0e5b-49d7-85bc-1e6ed558f9bc'
+          f.jsonKey = 'massWeight'
+          f.name = 'Mass Weight'
+          f.description = 'Mass weight in kg'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end,
+        BjondApi::BjondField.new.tap do |f|
+          f.id = 'cbbec641-0c8c-4326-ae7b-467dab0218a5'
+          f.jsonKey = 'bmi'
+          f.name = 'Body Mass Index'
+          f.description = 'Body mass index (kg/cm^2)'
+          f.fieldType = 'Number'
+          f.event = e.id
+        end
+      ]
     end
   ]
 end
